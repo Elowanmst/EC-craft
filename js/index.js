b90cbe22414f6682342af0d8250c7b30d6e721d2
Elowan MESTRES <elowanmestres@gmail.com>
2025-02-05 15:43:54 +0100

    commit intermediaire

--- js/index.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.salut').forEach(salut => {
-    salut.addEventListener('click', () => {
-        const card = salut.querySelector('.card');
-        card.classList.toggle('flipped');
-    });
-});
