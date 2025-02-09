dd6821e08e39fab79ea77f5cbd874dedaa823d88
Cam- <veillardcamille01@gmail.com>
2025-02-09 18:31:41 +0100

    responsive fixes

--- js/index.js
+++ js/index.js
@@ -33,4 +33,22 @@ jQuery(function($) {
     $(window).on('scroll', doAnimations);
     $(window).trigger('scroll');
   
-  });
+});
+
+
+window.onload = function () {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+
+    console.log("Burger:", burger);
+    console.log("Nav:", nav);
+
+    if (burger && nav) {
+        burger.addEventListener("click", () => {
+            nav.classList.toggle("nav-active");
+            burger.classList.toggle("toggle");
+        });
+    } else {
+        console.error("Erreur : l'élément burger ou nav-links est introuvable.");
+    }
+};
